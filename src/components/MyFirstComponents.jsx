f6ec56f28e3104eb20e4173ccc9a1e8e75f06b7a
CamiloRamirez <126025262+CamiloBETA6@users.noreply.github.com>
2024-09-25 22:14:09 -0500

    My First Components

--- src/components/MyFirstComponents.jsx
+++ src/components/MyFirstComponents.jsx
@@ -1,14 +1,30 @@
 export const MyFirstComponents = () => {
+  const name = "Camilo Ramirez Martinez";
+
+  const github_profile = "https://github.com/CamiloBETA6";
+
   return (
     <>
       <div>MyFirstComponents</div>
-      <h1>Temas de React</h1>
-      <ul>
-        <li>Componentes</li>
-        <li>Eventos</li>
-        <li>Estados Hooks</li>
-        <li>Props</li>
-      </ul>
+      <div className="container bg-primary">
+        <h1>Temas de React</h1>
+        <ul className="list-padre">
+          <li>Componentes</li>
+          <li>Eventos</li>
+          <li>Estados Hooks</li>
+          <li>Props</li>
+        </ul>
+      </div>
+      <div className="container bg-warning">
+        <h1>Temas de React</h1>
+        <p>Datos de estudiante: {name}</p>
+        <p>
+          My github is,
+          <a className="text-black" href={github_profile}>
+            CamiloBETA6
+          </a>
+        </p>
+      </div>
     </>
   );
 };
