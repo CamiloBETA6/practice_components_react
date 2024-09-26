f45f5acfebad73d3dce746f5d3247e288414aa55
CamiloRamirez <126025262+CamiloBETA6@users.noreply.github.com>
2024-09-25 21:13:59 -0500

    My First Components

---
+++ src/components/MyFirstComponents.jsx
@@ -0,0 +1,14 @@
+export const MyFirstComponents = () => {
+  return (
+    <>
+      <div>MyFirstComponents</div>
+      <h1>Temas de React</h1>
+      <ul>
+        <li>Componentes</li>
+        <li>Eventos</li>
+        <li>Estados Hooks</li>
+        <li>Props</li>
+      </ul>
+    </>
+  );
+};
